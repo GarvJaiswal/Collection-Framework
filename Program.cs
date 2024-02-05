e1a19d549a47e1c14e5adefbac8546f05c399936
GarvJaiswal <garvjaiswal08@gmail.com>
2024-02-05 09:44:19 +0530

    [Garv] Add:Basic methods of stack implemented

--- Program.cs
+++ Program.cs
@@ -1,65 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
 
+class IntStackCRUD
+{
+    private Stack<int> stack;
+
+    public IntStackCRUD()
+    {
+        stack = new Stack<int>();
+    }
+
+    public void Create(int item)
+    {
+        stack.Push(item);
+    }
+
+    public int Read()
+    {
+        if (stack.Count > 0)
+            return stack.Peek();
+        else
+            throw new InvalidOperationException("Stack is empty");
+    }
+
+    public void Update(int newItem)
+    {
+        if (stack.Count > 0)
+        {
+            stack.Pop();
+            stack.Push(newItem);
+        }
+        else
+        {
+            throw new InvalidOperationException("Stack is empty");
+        }
+    }
+
+    public int Delete()
+    {
+        if (stack.Count > 0)
+            return stack.Pop();
+        else
+            throw new InvalidOperationException("Stack is empty");
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        // Create a new list
-        List<string> myList = new List<string>();
+        IntStackCRUD myStack = new IntStackCRUD();
 
-        // Add some initial items
-        myList.Add("Item 1");
-        myList.Add("Item 2");
-        myList.Add("Item 3");
+        // Create
+        myStack.Create(1);
+        myStack.Create(2);
+        myStack.Create(3);
 
-        // Read (Retrieve) items
-        Console.WriteLine("Items in the list:");
-        foreach (var item in myList)
-        {
-            Console.WriteLine(item);
-        }
+        // Read
+        Console.WriteLine("Top item in the stack: " + myStack.Read());
 
-        // Update an item
-        int indexToUpdate = 1;
-        if (indexToUpdate >= 0 && indexToUpdate < myList.Count)
-        {
-            myList[indexToUpdate] = "Updated Item";
-            Console.WriteLine($"Item at index {indexToUpdate} has been updated.");
-        }
-        else
-        {
-            Console.WriteLine("Invalid index for update.");
-        }
+        // Update
+        myStack.Update(4);
+        Console.WriteLine("Top item in the stack after update: " + myStack.Read());
 
-        // Delete an item
-        int indexToDelete = 0;
-        if (indexToDelete >= 0 && indexToDelete < myList.Count)
-        {
-            myList.RemoveAt(indexToDelete);
-            Console.WriteLine($"Item at index {indexToDelete} has been deleted.");
-        }
-        else
-        {
-            Console.WriteLine("Invalid index for deletion.");
-        }
-
-        // Read (Retrieve) items after deletion
-        Console.WriteLine("Items in the list after deletion:");
-        foreach (var item in myList)
-        {
-            Console.WriteLine(item);
-        }
-
-        // Add a new item
-        myList.Add("New Item");
-        Console.WriteLine("New item added to the list.");
-
-        // Read (Retrieve) items after addition
-        Console.WriteLine("Items in the list after addition:");
-        foreach (var item in myList)
-        {
-            Console.WriteLine(item);
-        }
+        // Delete
+        Console.WriteLine("Deleted item from the stack: " + myStack.Delete());
+        Console.WriteLine("Top item in the stack after deletion: " + myStack.Read());
     }
 }
